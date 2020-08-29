93a262cac1c2a1298ea0905577df0d2decc3d32e
Isuru Anuhas <isuruanuhasrajapaksha@gmail.com>
2020-08-29 23:41:42 +0530

    web_api_project_login
    implemented login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import fire from './config/Fire';
+import LoginRegister from './components/LoginRegister';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+
+  constructor()
+  {
+    super();
+    this.state = {
+      user :  null
+    }
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
+
+  authListener()
+  {
+    fire.auth().onAuthStateChanged((user) =>
+    {
+      if(user){
+        this.setState({user});
+      }
+      else{
+        this.setState({user:null});
+      }
+    }); 
+  }
+
+  render()
+  {
+    return(
+      <div>
+        {this.state.user? (<Home />) : (<LoginRegister />)}
+        </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/config/Fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBeRB9WdLVrwuXbVknh3t3orIeaq5IkNLs",
+    authDomain: "fir-shoppingcart-2f935.firebaseapp.com",
+    databaseURL: "https://fir-shoppingcart-2f935.firebaseio.com",
+    projectId: "fir-shoppingcart-2f935",
+    storageBucket: "fir-shoppingcart-2f935.appspot.com",
+    messagingSenderId: "362134882252",
+    appId: "1:362134882252:web:1624653d48ced093a77f69",
+    measurementId: "G-RB3LSY2JDS"
+  };
+
+  const fire = firebase.initializeApp(firebaseConfig);
+  export default fire; 
