0d01ec2b3448df5ec424ee169fc08163fdaa6e8d
Dinuka_T <39722537+dinuka99@users.noreply.github.com>
2020-09-02 22:27:55 +0530

    TuesDayBootstrap

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -1,5 +1,5 @@
 import React ,{Component} from  "react"
-import Hero from "./ItemOrderView";
+import Hero from "./Hero";
 
 class Heroes extends Component {
 
@@ -9,29 +9,68 @@ class Heroes extends Component {
     render( ) {
 return (
 
-    <div class="container">
-    <div class="row">
-    <div class="col">
-        <Hero/>
+
+
+
+
+
+
+
+    <div className="fixed-top">
+    <div className="collapse" id="navbarToggleExternalContent">
+      <div className="bg-dark p-4">
+        <h5 className="text-white h4">Collapsed content</h5>
+        <span className="text-muted">Toggleable via the navbar brand.</span>
+      </div>
     </div>
-    <div class="col">
-        <Hero/>
-    </div>
-    <div class="col">
-        <Hero/>
-    </div>
-    </div>
-     </div>
-
-);
-      
+    <nav className="navbar navbar-dark bg-dark">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  
 
 
- 
-    
 
 
-    };  
-}
+
+
+
+
+
+    <div className="container">
+<div className="row">
+<div className="col">
+    <Hero/>
+</div>
+<div className="col">
+    <Hero/>
+</div>
+<div className="col">
+    <Hero/>
+</div>
+
+</div>
+ </div>
+
+
+
+<div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups"/>
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+    <button type="button" class="btn btn-secondary">1</button>
+    <button type="button" class="btn btn-secondary">2</button>
+    <button type="button" class="btn btn-secondary">3</button>
+    <button type="button" class="btn btn-secondary">4</button>
+  //</div></div>
+
+
+
+
+
+
+
+    );
+        }
+    }
     
 export default Heroes;
