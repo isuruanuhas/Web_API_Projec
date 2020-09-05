e4d1a326d73401503d3e24cac5b378144cf2cf93
Dinuka_T <39722537+dinuka99@users.noreply.github.com>
2020-09-05 15:48:05 +0530

    ChangeJSON-FileFrontEndHasSomeErrors

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import fire from '../config/Fire';
+import '../App.css';
+
+class Home extends Component(){
+render() {
+
+    return(
+
+        <div>
+            <h2>Home Page</h2>
+        </div>
+
+    );
+
+}}
+export default Home
